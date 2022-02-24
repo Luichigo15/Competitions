07d0bafc1e7e29298affa32fdf44d6904a33b3c1
Luichigo <grovile11@hotmai.com>
2022-02-24 13:41:50 -0600

    Hash code

--- HashCode/2022/QualificationRound/mentorship_and_teamwork.dart
+++ HashCode/2022/QualificationRound/mentorship_and_teamwork.dart
@@ -13,7 +13,7 @@ class Solution{
     ];
 
     final data =
-        await readFile('${Directory.current.path}/$directory/${examples[0]}');
+        await readFile('${Directory.current.path}/$directory/${examples[1]}');
 
     List<Contributors> contributors = [];
     List<Project> projects = [];
@@ -52,8 +52,66 @@ class Solution{
         ));
     }
 
-    print(contributors);
-    print(projects);
+    projects.sort((element1,element2)=>element1.roles.length.compareTo(element2.roles.length));
+
+    final resultData = calculateData(contributors, projects);
+    print(resultData);
+    writeFile(resultData, '${Directory.current.path}/$directory/result1.txt');
+  }
+
+  String calculateData(List<Contributors> cont,List<Project> projects){
+    int k = 0;
+    List<Project> projectsFinal = [];
+    while(projects.where((element) => !element.isFullRoles).length>0){
+      if(k>=projects.length) k=0;
+      final project = projects[k];
+      for (var i = 0; i < project.roles.length; i++) {
+        final list = cont.where(
+          (contri) => contri.skill.where(
+            (skill) => skill.name == project.roles[i].name && skill.level >= project.roles[i].level).length>0).toList();
+
+        if(list.length==0) continue;
+
+        for (var j = 0; j < list.length; j++) {
+          if(project.contributors.contains(list[j])) continue;
+          project.contributors.add(list[j]);
+          break;
+        }
+      }
+
+      if(project.contributors.length==project.roles.length){
+        project.isFullRoles = true;
+        project.contributors.forEach((element) {
+          cont[cont.indexOf(element)].skill.forEach((skill) {skill.level++; });
+        });
+        projectsFinal.add(project);
+      }
+      k++;
+    }
+
+    String result = '${projectsFinal.length}\n';
+    for (var i = 0; i < projectsFinal.length; i++) {
+      final element = projectsFinal[i];
+      result+='${element.name}\n';
+      String names = '';
+      element.contributors.forEach((contri) {
+        if(names.isEmpty){
+          names+= '${contri.name}';
+        }else{
+          names+= ' ${contri.name}';
+        }
+        
+      });
+
+      if(i==projectsFinal.length-1){
+        result+='$names';
+      }else{
+        result+='$names\n';
+      }
+      
+    }
+
+    return result;
   }
 
   Future<List<String>> readFile(String path) async {
@@ -93,6 +151,8 @@ class Project{
   int score;
   int bestDay;
   List<Skill> roles;
+  List<Contributors> contributors = [];
+  bool isFullRoles = false;
 
   Project(this.name,
   this.days,
@@ -107,6 +167,7 @@ class Project{
         'score':this.score,
         'bestDay':this.bestDay,
         'roles':this.roles.toString(),
+        'contributors':this.contributors.toString()
       }.toString();
     }
 }
